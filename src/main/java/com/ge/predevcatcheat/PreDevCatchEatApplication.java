01ef667e3dd647e4fa826464b8234237e3866854
ChaewonHan <chwonhan00@gmail.com>
2025-02-25 07:59:43 +0900

    first commit

---
+++ src/main/java/com/ge/predevcatcheat/PreDevCatchEatApplication.java
@@ -0,0 +1,13 @@
+package com.ge.predevcatcheat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PreDevCatchEatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PreDevCatchEatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ge/predevcatcheat/PreDevCatchEatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ge.predevcatcheat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PreDevCatchEatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
